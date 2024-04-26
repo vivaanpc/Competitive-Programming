90f68cb35f7bcee4d3e188470057cd6a93c155dd
Vivaan <vivaanpchheda@gmail.com>
2024-04-27 00:44:04 +0530

    sqrt x

---
+++ LeetCode/58. Length of Last Word.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+	int lengthOfLastWord(string s) {
+		reverse(s.begin(), s.end());
+		int count = 0;
+		for (char c : s)
+		{
+			if (c == ' ' && count == 0) {
+				continue;
+			}
+			else if (c == ' ' && count > 0) {
+				break;
+			}
+			else {
+				count++;
+			}
+		}
+		return count;
+	}
+};
