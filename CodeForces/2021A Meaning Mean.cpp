f03fea1f37ae0bd1310f3cb9f4289554bf5447ba
Vivaan <vivaanpchheda@gmail.com>
2024-10-11 06:26:30 +0530

    Daily Streak

---
+++ CodeForces/2021A Meaning Mean.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+//vivaanpc
+using namespace std;
+//vivaanpc
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int t;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+		vector<int> a(n);
+		for (int i = 0; i < n; i++) {
+			cin >> a[i];
+		}
+		sort(a.begin(), a.end());
+		int ans = a[0];
+		for (int i = 1; i < n; i++) {
+			ans = (ans + a[i]) / 2;
+		}
+		cout << ans << "\n";
+	}
+}
