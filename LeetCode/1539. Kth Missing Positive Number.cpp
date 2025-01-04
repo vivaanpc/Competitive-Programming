07af7faa4c7c3d0b9d82d3f4c265323e09b464d1
Vivaan <vivaanpchheda@gmail.com>
2025-01-04 21:56:04 +0530

    Kth Missing +ve number

---
+++ LeetCode/1539. Kth Missing Positive Number.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+//vivaanpc
+using namespace std;
+//vivaanpc
+typedef long long ll;
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int low = 0;
+        int high = arr.size() - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] - mid - 1 < k) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return k + high + 1;
+    }
+};
