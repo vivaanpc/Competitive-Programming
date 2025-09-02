293dbb7a714d6838eea0dcd0f2e3da1562592105
Vivaan <vivaanpchheda@gmail.com>
2025-09-02 15:13:33 +0530

    LC Fibonacci Number O(1) using dp

--- LeetCode/509. Fibonacci Number.cpp
+++ LeetCode/509. Fibonacci Number.cpp
@@ -1,10 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
+const int MAX_SAVE = 3;
 class Solution {
 public:
+	int f[MAX_SAVE];
 	int fib(int n) {
-		if (n == 0) return 0;
-		if (n <= 2) return 1;
-		else return fib(n - 2) + fib(n - 1);
+		f[0] = 0;
+		f[1] = 1;
+		for (int i = 2; i <= n; i++) {
+			f[i % MAX_SAVE] = f[(i - 1) % MAX_SAVE] + f[(i - 2) % MAX_SAVE];
+		}
+		return f[n % MAX_SAVE];
 	}
 };
