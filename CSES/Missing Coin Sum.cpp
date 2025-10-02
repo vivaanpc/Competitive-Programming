8cb41bbd24721e7b6173b8e567bc7d1621872ac7
Vivaan <vivaanpchheda@gmail.com>
2025-10-02 15:22:42 +0530

    CSES Missing Coin Sum

---
+++ CSES/Missing Coin Sum.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+//vivaanpc
+using namespace std;
+//vivaanpc
+typedef long long ll;
+const int mod = 1e9 + 7;
+const double eps = 1e-12;
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+    sort(v.begin(), v.end());
+    ll currentSum = 1;
+    for (int i = 0; i < n; ++i) {
+        if (currentSum < v[i]) {
+            break;
+        } else {
+            currentSum += v[i];
+        }
+    }
+    cout << currentSum;
+}
+//vivaanpc
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
