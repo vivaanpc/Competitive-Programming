983479b03736cf538ac00e2cf58c735dda751b34
Vivaan <vivaanpchheda@gmail.com>
2025-10-06 17:38:18 +0530

    CF 2115A

---
+++ CodeForces/2115A El fucho.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+//vivaanpc
+using namespace std;
+//vivaanpc
+typedef long long ll;
+const int mod = 1e9 + 7;
+const double eps = 1e-12;
+void solve() {
+    int n;
+    cin >> n;
+    cout << (n - 1) * 2 << '\n';
+
+}
+//vivaanpc
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
