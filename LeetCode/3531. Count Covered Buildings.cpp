a78b19a617b9479c66b4cd017e0e7a107acc8b85
Vivaan <vivaanpchheda@gmail.com>
2025-12-12 16:01:28 +0530

    LC 3531 CF 2176B

--- LeetCode/3531. Count Covered Buildings.cpp
+++ LeetCode/3531. Count Covered Buildings.cpp
@@ -1,79 +1,38 @@
 #include<bits/stdc++.h>
-//Hi Stalker
+//vivaanpc
 using namespace std;
-#ifndef ONLINE_JUDGE
-#include "algo/debug.h"
-#else
-#define debug(...) 42
-#endif
-//Hi Stalker
+// #ifndef ONLINE_JUDGE
+// #include "algo/debug.h"
+// #else
+// #define debug(...) 42
+// #endif
+//vivaanpc
 typedef long long ll;
 const int mod = 1e9 + 7;
 const double eps = 1e-12;
-void solve() {
-    int n;
-    cin >> n;
-    vector<int> v(n), odd, even, ans;
-    for (int i = 0; i < n; i++) {
-        cin >> v[i];
-        if (v[i] & 1) odd.push_back(v[i]);
-        else even.push_back(v[i]);
-    }
-    if (odd.empty()) {
-        for (int i = 0; i < n; i++) {
-            cout << "0 ";
+class Solution {
+public:
+    int countCoveredBuildings(int n, vector<vector<int>>& buildings) {
+        vector<int> Xmin(n + 1, n + 1);
+        vector<int> Xmax(n + 1, 0);
+        vector<int> Ymin(n + 1, n + 1);
+        vector<int> Ymax(n + 1, 0);
+        for (auto b : buildings) {
+            int x = b[0];
+            int y = b[1];
+            Xmin[y] = min(Xmin[y], x);
+            Xmax[y] = max(Xmax[y], x);
+            Ymin[x] = min(Ymin[x], y);
+            Ymax[x] = max(Ymax[x], y);
         }
-        cout << "\n";
-        return;
-    }
-    if (even.empty()) {
-        sort(odd.begin(), odd.end(), greater<int>());
-        for (int i = 0; i < n; i++) {
-            if (i % 2 == 0) {
-                cout << odd[0] << " ";
-            } else {
-                cout << "0 ";
+        int ans = 0;
+        for (auto b : buildings) {
+            int x = b[0];
+            int y = b[1];
+            if (Xmin[y] < x && x < Xmax[y] && Ymin[x] < y && y < Ymax[x]) {
+                ans += 1;
             }
         }
-        cout << "\n";
-        return;
+        return ans;
     }
-    sort(odd.begin(), odd.end(), greater<int>());
-    sort(even.begin(), even.end(), greater<int>());
-    if (odd.size() == 2 && even.size() == 1) {
-        cout << odd[0] << " " << odd[0] + even[0] << " " << "0\n";
-        return;
-    }
-    int a = 0, b = 0;
-    ans.push_back(odd[b]);
-    ans.push_back(even[a] + odd[b]);
-    a++;
-    b++;
-    while (a < even.size()) {
-        ans.push_back(ans.back() + even[a++]);
-    }
-    int aSize = ans.size();
-    ans.resize(n);
-    if (aSize == n) {
-        for (const auto i : ans) cout << i << " ";
-        cout << "\n";
-        return;
-    }
-    while (aSize != n) {
-        ans[aSize] = ans[aSize - 2];
-        aSize++;
-    }
-    for (const auto i : ans) cout << i << " ";
-    cout << "\n";
-}
-//Hi Stalker
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int t;
-    cin >> t;
-    while (t--) {
-        solve();
-    }
-    return 0;
-}
+};
