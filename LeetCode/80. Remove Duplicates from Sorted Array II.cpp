00f15cd86a8ffc17d96d568086a78ae6b14fe151
Vivaan <vivaanpchheda@gmail.com>
2025-12-25 23:22:14 +0530

    LC 80

---
+++ LeetCode/80. Remove Duplicates from Sorted Array II.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+//vivaanpc
+using namespace std;
+// #ifndef ONLINE_JUDGE
+// #include "algo/debug.h"
+// #else
+// #define debug(...) 42
+// #endif
+//vivaanpc
+typedef long long ll;
+const int mod = 1e9 + 7;
+const double eps = 1e-12;
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int k = 2;
+
+        if (nums.size() <= 2) return nums.size();
+
+        for (int i = 2; i < nums.size(); i++) {
+            if (nums[i] != nums[k - 2]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+};
