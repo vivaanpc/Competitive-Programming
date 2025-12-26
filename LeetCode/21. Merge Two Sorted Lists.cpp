19fe50d6e7affd82ec0db0cfe444f58432807860
Vivaan <vivaanpchheda@gmail.com>
2025-12-27 01:24:20 +0530

    LC 142 21

---
+++ LeetCode/21. Merge Two Sorted Lists.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+//vivaanpc
+using namespace std;
+// #ifndef ONLINE_JUDGE
+// #include "algo/debug.h"
+// #else
+// #define debug(...) 42
+// #endif
+//vivaanpc
+typedef long long ll;
+const int mod = 1e9 + 7;
+const double eps = 1e-12;
+// Definition for singly-linked list. Comment out
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
