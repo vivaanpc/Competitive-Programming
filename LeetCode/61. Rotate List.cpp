8d02e7d7a0cc35ec017234c2d681e7bb31b406bb
Vivaan <vivaanpchheda@gmail.com>
2025-12-26 20:32:58 +0530

    LC 61

---
+++ LeetCode/61. Rotate List.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+//vivaanpc
+using namespace std;
+// #ifndef ONLINE_JUDGE
+// #include "algo/debug.h"
+// #else
+// #define debug(...) 42
+// #endif
+//vivaanpc
+typedef long long ll;
+const int mod = 1e9 + 7;
+const double eps = 1e-12;
+// Definition for singly-linked list. Comment out
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        int len = 0;
+        ListNode* temp = head;
+        while (temp != nullptr) {
+            len++;
+            temp = temp -> next;
+        }
+        k = len % k;
+        if (k == 0) return head;
+
+    }
+};
