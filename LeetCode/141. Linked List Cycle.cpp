a2624c3f62c94874828fc697c25c858fb1de00bc
Vivaan <vivaanpchheda@gmail.com>
2025-12-26 16:46:51 +0530

    LC141

---
+++ LeetCode/141. Linked List Cycle.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+//vivaanpc
+using namespace std;
+// #ifndef ONLINE_JUDGE
+// #include "algo/debug.h"
+// #else
+// #define debug(...) 42
+// #endif
+//vivaanpc
+typedef long long ll;
+const int mod = 1e9 + 7;
+const double eps = 1e-12;
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+
+    }
+};
