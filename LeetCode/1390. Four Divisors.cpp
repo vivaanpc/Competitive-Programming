084182ab930c8519c2d7e4a0ae5f6038eb4871cd
Vivaan <vivaanpchheda@gmail.com>
2026-01-04 18:20:31 +0530

    LC 1390 0ms Optimized

--- LeetCode/1390. Four Divisors.cpp
+++ LeetCode/1390. Four Divisors.cpp
@@ -12,22 +12,21 @@ const int mod = 1e9 + 7;
 const double eps = 1e-12;
 class Solution {
 public:
-    int getDivisors(int &n) {
-        int ans = 0, sum = 0;
+    int getDivisors(int& n) {
+        int cnt = 0, sum = 0;
         for (int i = 1; i * i <= n; i++) {
-            if (ans > 4) return 0;
-            if (i * i == n && i * i == n) {
-                ans++;
-                sum += n;
-                break;
-            }
             if (n % i == 0) {
-                ans += 2;
-                sum += i + n / i;
+                if (i * i == n) {
+                    cnt++;
+                    sum += i;
+                } else {
+                    cnt += 2;
+                    sum += i + n / i;
+                }
+                if (cnt > 4) return 0;
             }
         }
-        if (ans == 4) return sum;
-        return 0;
+        return cnt == 4 ? sum : 0;
     }
     int sumFourDivisors(vector<int>& v) {
         ll ans = 0;
