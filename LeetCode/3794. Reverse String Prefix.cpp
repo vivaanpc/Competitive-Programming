afd0f78c86cfbe269b1cda124460e0a449ce94c0
Vivaan <vivaanpchheda@gmail.com>
2026-01-11 01:38:20 +0530

    LC 3794

---
+++ LeetCode/3794. Reverse String Prefix.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+//vivaanpc
+using namespace std;
+#ifndef ONLINE_JUDGE
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+//vivaanpc
+typedef long long ll;
+const int mod = 1e9 + 7;
+const double eps = 1e-12;
+class Solution {
+public:
+	string reversePrefix(string s, int k) {
+		if (s.length() == k) {
+			reverse(s.begin(), s.end());
+		}
+	}
+};
