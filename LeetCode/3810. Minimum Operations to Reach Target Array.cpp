5a416464a6c223894987dfbd1adce815b3cf19d8
Vivaan <vivaanpchheda@gmail.com>
2026-01-17 23:38:15 +0530

    LC 3810

---
+++ LeetCode/3810. Minimum Operations to Reach Target Array.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+//vivaanpc
+using namespace std;
+#ifndef ONLINE_JUDGE
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+//vivaanpc
+typedef long long ll;
+const int mod = 1e9 + 7;
+const double eps = 1e-12;
+class Solution {
+public:
+	int minOperations(vector<int>& nums, vector<int>& target) {
+
+	}
+};
