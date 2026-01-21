78b66abda622082f2ebd7216c92f03fd49b7db95
Vivaan <vivaanpchheda@gmail.com>
2026-01-21 17:46:04 +0530

    LC 3315

---
+++ LeetCode/3315. Construct the Minimum Bitwise Array II.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+//vivaanpc
+using namespace std;
+#ifndef ONLINE_JUDGE
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+//vivaanpc
+typedef long long ll;
+const int mod = 1e9 + 7;
+const double eps = 1e-12;
+void solve() {
+	int n;
+	cin >> n;
+	vector<int> v(n);
+	for (int i = 0; i < n; i++) {
+		cin >> v[i];
+	}
+
+}
+//vivaanpc
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int t;
+	cin >> t;
+	while (t--) {
+		solve();
+	}
+	return 0;
+}
