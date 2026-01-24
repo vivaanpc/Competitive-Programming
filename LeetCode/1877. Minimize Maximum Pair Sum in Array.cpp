52b58ed84206a1af1f1251523843555a389fd7e2
Vivaan <vivaanpchheda@gmail.com>
2026-01-24 21:33:20 +0530

    LC 1877

---
+++ LeetCode/1877. Minimize Maximum Pair Sum in Array.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+//vivaanpc
+using namespace std;
+#ifndef ONLINE_JUDGE
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+//vivaanpc
+typedef long long ll;
+const int mod = 1e9 + 7;
+const double eps = 1e-12;
+class Solution {
+public:
+	int minPairSum(vector<int>& v) {
+		sort(v.begin(), v.end());
+		int maxi = INT_MIN;
+		for (int i = 0; i < (int)(v.size() / 2); i++) {
+			maxi = max(maxi, v[i] + v[v.size() - i - 1]);
+		}
+		return maxi;
+	}
+};
