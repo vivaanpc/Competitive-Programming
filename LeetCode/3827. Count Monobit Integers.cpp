cd65718ecd2e0ed52b7594b5769f144df3f75a24
Vivaan <vivaanpchheda@gmail.com>
2026-02-04 03:35:41 +0530

    LC 3827

---
+++ LeetCode/3827. Count Monobit Integers.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+//vivaanpc
+using namespace std;
+// #ifndef ONLINE_JUDGE
+// #include "algo/debug.h"
+// #else
+// #define debug(...) 42
+// #endif
+//vivaanpc
+typedef long long ll;
+const int mod = 1e9 + 7;
+const double eps = 1e-12;
+class Solution {
+public:
+    int countMonobit(int n) {
+        if (n == 1) return 2;
+        return log2(n + 1) + 1;
+    }
+};
