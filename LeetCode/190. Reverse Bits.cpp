e76b88fcb2ccab0373575bcfe88dd8c9fafd62a4
Vivaan <vivaanpchheda@gmail.com>
2026-02-16 16:22:07 +0530

    LC 190

---
+++ LeetCode/190. Reverse Bits.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+//vivaanpc
+using namespace std;
+// #ifndef ONLINE_JUDGE
+// #include "algo/debug.h"
+// #else
+// #define debug(...) 42
+// #endif
+//vivaanpc
+typedef long long ll;
+const int mod = 1e9 + 7;
+const double eps = 1e-12;
+class Solution {
+public:
+    int reverseBits(int n) {
+        int ans = 0;
+        for (int i = 0; i < 32; i++) {
+            ans <<= 1;
+            ans |= (n & 1);
+            n >>= 1;
+        }
+        return ans;
+    }
+};
